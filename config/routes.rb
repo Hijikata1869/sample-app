0c570661ee50b292e55b8295499eae1ef570c2ce
Hijikata1869 <liv.knk@gmail.com>
2021-07-08 20:10:51 +0900

    サーバーサイドのルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
-  namespace :v1 do
-    mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+    }
+      resources :users
+      resources :questions
+
+      resources :training_logs do
+        resource :likes, only: [:create, :destroy]
+      end
+      resources :advices do
+        resource :bookmarks, only: [:create, :destroy]
+      end
+
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
